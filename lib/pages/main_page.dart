7eb2b91eeeef33e4e27ea61581fe61b322a4eb3c
devmaslove <63534992+devmaslove@users.noreply.github.com>
2022-08-29 20:52:37 +0300

    Добавлена виджет заголовка с оранжевой кнопкой

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,4 +1,6 @@
+import 'package:ecommerce_test/resources/app_colors.dart';
 import 'package:ecommerce_test/resources/app_images.dart';
+import 'package:ecommerce_test/resources/app_text_style.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -17,15 +19,68 @@ class MainPage extends StatelessWidget {
               height: 13,
             ),
             tooltip: 'Open filter',
-            onPressed: () {
-              // handle the press
-            },
+            onPressed: () {},
           ),
+          const SizedBox(width: 17),
         ],
       ),
       body: ListView(
-        children: const [],
+        children: [
+          ListTitleWidget(
+            title: 'Select Category',
+            buttonText: 'view all',
+            onPressed: () {},
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class ListTitleWidget extends StatelessWidget {
+  final String title;
+  final String buttonText;
+  final VoidCallback onPressed;
+
+  const ListTitleWidget({
+    super.key,
+    required this.title,
+    required this.buttonText,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const SizedBox(width: 17),
+        Text(
+          title,
+          style: const AppTextStyle().copyWith(
+            fontSize: 25,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        const Spacer(),
+        TextButton(
+          onPressed: onPressed,
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
+            ),
+          ),
+          child: Text(
+            buttonText,
+            style: const AppTextStyle().copyWith(
+              fontSize: 15,
+              fontWeight: FontWeight.w400,
+              color: AppColors.accent,
+            ),
+          ),
+        ),
+        const SizedBox(width: 17),
+      ],
     );
   }
 }
