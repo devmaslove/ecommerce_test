bb3981c66fca2b33f0849abc29d6c95de47296a5
devmaslove <63534992+devmaslove@users.noreply.github.com>
2022-08-29 22:35:30 +0300

    В цвета добавлены коментарии чтобы было видно что за цвет при наведении

--- lib/resources/app_colors.dart
+++ lib/resources/app_colors.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 class AppColors {
   AppColors._();
 
+  /// #FF6E4E
   static const accent = Color(0xFFFF6E4E);
+
+  /// #010035
   static const dark = Color(0xFF010035);
+
+  /// #E5E5E5
   static const background = Color(0xFFE5E5E5);
 }
