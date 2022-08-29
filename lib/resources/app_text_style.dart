d64ebe627b3871394e96565f7444fa7af7bc052b
devmaslove <63534992+devmaslove@users.noreply.github.com>
2022-08-29 13:08:37 +0300

    Добавлены шрифты из макета

---
+++ lib/resources/app_text_style.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyle extends TextStyle {
+  const AppTextStyle()
+      : super(
+          fontFamily: 'Mark Pro',
+          color: Colors.black,
+          letterSpacing: -0.33,
+          height: 1.1,
+        );
+}
