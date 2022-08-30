e38b9813e4da4229ad87adc15b2e5851fa08c083
devmaslove <63534992+devmaslove@users.noreply.github.com>
2022-08-30 22:44:42 +0300

    Добавил переключение категорий

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -4,8 +4,15 @@ import 'package:ecommerce_test/resources/app_text_style.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  String selectedCategory = 'Phones';
 
   @override
   Widget build(BuildContext context) {
@@ -31,57 +38,78 @@ class MainPage extends StatelessWidget {
             buttonText: 'view all',
             onPressed: () {},
           ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 12,
-                horizontal: 15,
-              ),
-              child: Row(
-                children: [
-                  CategoryWidget(
-                    title: 'Phones',
-                    selected: true,
-                    imageAssetName: AppImages.phones,
-                    imageWidth: 18.33,
-                    imageHeight: 30,
-                    onPressed: () {},
-                  ),
-                  CategoryWidget(
-                    title: 'Computer',
-                    selected: false,
-                    imageAssetName: AppImages.computer,
-                    imageWidth: 29,
-                    imageHeight: 31,
-                    onPressed: () {},
-                  ),
-                  CategoryWidget(
-                    title: 'Health',
-                    selected: false,
-                    imageAssetName: AppImages.health,
-                    imageWidth: 32,
-                    imageHeight: 30,
-                    onPressed: () {},
-                  ),
-                  CategoryWidget(
-                    title: 'Books',
-                    selected: false,
-                    imageAssetName: AppImages.books,
-                    imageWidth: 21,
-                    imageHeight: 28.06,
-                    onPressed: () {},
-                  ),
-                  CategoryWidget(
-                    title: 'Other',
-                    selected: false,
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-            ),
+          ListCategoriesWidget(
+            selectedCategory: selectedCategory,
+            onSelectCategory: (String category) {
+              setState(() => selectedCategory = category);
+            },
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ListCategoriesWidget extends StatelessWidget {
+  final String selectedCategory;
+  final ValueChanged<String> onSelectCategory;
+
+  const ListCategoriesWidget({
+    super.key,
+    required this.selectedCategory,
+    required this.onSelectCategory,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 12,
+          horizontal: 15,
+        ),
+        child: Row(
+          children: [
+            CategoryWidget(
+              title: 'Phones',
+              selected: selectedCategory == 'Phones',
+              imageAssetName: AppImages.phones,
+              imageWidth: 18.33,
+              imageHeight: 30,
+              onPressed: () => onSelectCategory('Phones'),
+            ),
+            CategoryWidget(
+              title: 'Computer',
+              selected: selectedCategory == 'Computer',
+              imageAssetName: AppImages.computer,
+              imageWidth: 29,
+              imageHeight: 31,
+              onPressed: () => onSelectCategory('Computer'),
+            ),
+            CategoryWidget(
+              title: 'Health',
+              selected: selectedCategory == 'Health',
+              imageAssetName: AppImages.health,
+              imageWidth: 32,
+              imageHeight: 30,
+              onPressed: () => onSelectCategory('Health'),
+            ),
+            CategoryWidget(
+              title: 'Books',
+              selected: selectedCategory == 'Books',
+              imageAssetName: AppImages.books,
+              imageWidth: 21,
+              imageHeight: 28.06,
+              onPressed: () => onSelectCategory('Books'),
+            ),
+            CategoryWidget(
+              title: 'Other',
+              selected: selectedCategory == 'Other',
+              onPressed: () => onSelectCategory('Other'),
+            ),
+          ],
+        ),
       ),
     );
   }
